9c769719e11df16f6419c46392a5dcc7b0e893d1
AhmadMdi <admimohammadi@gmail.com>
2023-12-22 22:16:58 +0330

    set resource

---
+++ app/Http/Resources/Admin/CategoryResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources\Admin;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CategoryResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+//        return parent::toArray($request);
+        return [
+          'title' => $this->title,
+//          'parent_id' => $this->whenLoaded('parent'),
+          'parent' => new CategoryResource($this->whenLoaded('parent')),
+          'children' => CategoryResource::collection($this->whenLoaded('children')),
+        ];
+    }
+}
